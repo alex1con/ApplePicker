74bfa2ba0a8e52d7ea9f9199752fd9657266eee6
Alex Smith <aps143@scarletmail.rutgers.edu>
2016-02-10 23:49:34 -0500

    Finished
    No High Score

---
+++ Assets/HighScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 1000;
+	void Start() {
+	// Assign the high score to ApplePickerHighScore
+	PlayerPrefs.SetInt("ApplePickerHighScore", score); // 3
+	}
+
+	void Awake() {
+		// If the ApplePickerHighScore already exists, read it
+		if (PlayerPrefs.HasKey ("ApplePickerHighScore"))
+			score = PlayerPrefs.GetInt ("ApplePickerHighScore");
+	}
+		
+	void Update () {
+		Text gt = this.GetComponent<Text>();
+		gt.text = "High Score: "+score;
+		// Update ApplePickerHighScore in PlayerPrefs if necessary
+		if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		
+	}
+
+}
